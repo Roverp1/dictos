beabb726eed823b0fcf9ae22e38def75e0592346
Roverp <yar.zubaha@proton.me>
2026-05-01 11:49:32 +0200

    feat(core): init CaptureRepository

---
+++ packages/core/src/ports/out/CaptureRepository.ts
@@ -0,0 +1,10 @@
+import type { Capture } from "@models/Capture";
+
+export interface CaptureRepository {
+  initialize(): Promise<void>;
+  save(
+    capture: Omit<Capture, "id" | "createdAt" | "modifiedAt">
+  ): Promise<Capture>;
+  findById(id: number): Promise<Capture | null>;
+  getAll(): Promise<Capture[] | null>;
+}
