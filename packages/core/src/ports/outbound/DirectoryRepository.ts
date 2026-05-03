0f6d1c2cf50deda6ad3e42e6335d70abf7f20738
Roverp <yar.zubaha@proton.me>
2026-05-03 13:04:55 +0200

    feat(core): init initial outbound ports

---
+++ packages/core/src/ports/outbound/DirectoryRepository.ts
@@ -0,0 +1,13 @@
+import type { Directory, NewDirectory } from "@models/Directory";
+import type { DbError } from "errors";
+
+export interface DirectoryRepository {
+  save(directory: NewDirectory): Promise<Directory | DbError>;
+  findById(id: number): Promise<Directory | DbError | null>;
+  findAll(): Promise<Directory[] | DbError>;
+  update(
+    id: number,
+    data: Partial<Omit<Directory, "id" | "createdAt" | "modifiedAt">>
+  ): Promise<Directory | DbError>;
+  delete(id: number): Promise<Directory | DbError>;
+}
---
+++ packages/core/src/ports/outbound/LlmPort.ts
@@ -0,0 +1,8 @@
+import type { LlmError } from "errors";
+
+export interface LlmPort {
+  generateDefinition(
+    captureText: string,
+    promptText: string
+  ): Promise<string | LlmError>;
+}
