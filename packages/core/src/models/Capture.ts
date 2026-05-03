cbb5247718474d59874b9b550165909702ad936c
Roverp <yar.zubaha@proton.me>
2026-05-03 12:33:15 +0200

    feat(core): create validation functions for domain models

--- packages/core/src/models/Capture.ts
+++ packages/core/src/models/Capture.ts
@@ -1,3 +1,5 @@
+import { ValidationError } from "errors";
+
 export interface Capture {
   id: number;
   text: string;
@@ -7,3 +9,11 @@ export interface Capture {
 }
 
 export type NewCapture = Omit<Capture, "id" | "createdAt" | "modifiedAt">;
+
+export function validateNewCapture(data: NewCapture): void | ValidationError {
+  if (!data.text || data.text.trim() === "")
+    return new ValidationError({ reason: "Capture cannot be empty." });
+
+  if (data.directoryId <= 0 || !Number.isInteger(data.directoryId))
+    return new ValidationError({ reason: "Invalid directory ID." });
+}
--- packages/core/src/models/Definition.ts
+++ packages/core/src/models/Definition.ts
@@ -1,3 +1,5 @@
+import { ValidationError } from "errors";
+
 export interface Definition {
   id: number;
   text: string;
@@ -7,3 +9,13 @@ export interface Definition {
 }
 
 export type NewDefinition = Omit<Definition, "id" | "createdAt" | "modifiedAt">;
+
+export function validateNewDefinition(
+  data: NewDefinition
+): void | ValidationError {
+  if (!data.text || data.text.trim() === "")
+    return new ValidationError({ reason: "Definition text cannot be empty." });
+
+  if (data.captureId <= 0 || !Number.isInteger(data.captureId))
+    return new ValidationError({ reason: "Invalid capture ID." });
+}
