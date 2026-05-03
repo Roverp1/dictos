443de95539da1bedaecc2ec539ff6e6219ffbfce
Roverp <yar.zubaha@proton.me>
2026-05-03 02:31:23 +0200

    feat(libsql): define full v1 schema

--- packages/adapters/libsql/src/schema.ts
+++ packages/adapters/libsql/src/schema.ts
@@ -5,17 +5,37 @@ import {
   text,
   type AnySQLiteColumn,
   check,
+  unique,
 } from "drizzle-orm/sqlite-core";
 
-export const capturesTable = sqliteTable("captures", {
-  id: int().primaryKey({ autoIncrement: true }),
-  text: text().notNull(), // text + directoryId primary key or unique constraint?
-  directoryId: int("directory_id").notNull(),
-  // .references(() => directoriesTable.id, { onDelete: "cascade" }),
-  createdAt: int("created_at", { mode: "timestamp" })
+export const capturesTable = sqliteTable(
+  "captures",
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    text: text().notNull(),
+    directoryId: int()
+      .notNull()
+      .references(() => directoriesTable.id, { onDelete: "cascade" }),
+    createdAt: int({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(strftime('%s', 'now'))`),
+    modifiedAt: int({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(strftime('%s', 'now'))`),
+  },
+  (t) => [unique().on(t.text, t.directoryId)]
+);
+
+export const definitionsTable = sqliteTable("definitions", {
+  id: int().primaryKey(),
+  captureId: int()
+    .notNull()
+    .references(() => capturesTable.id, { onDelete: "cascade" }),
+  text: text().notNull(),
+  createdAt: int({ mode: "timestamp" })
     .notNull()
     .default(sql`(strftime('%s', 'now'))`),
-  modifiedAt: int("modified_at", { mode: "timestamp" })
+  modifiedAt: int({ mode: "timestamp" })
     .notNull()
     .default(sql`(strftime('%s', 'now'))`),
 });
@@ -24,9 +44,10 @@ export const directoriesTable = sqliteTable(
   "directories",
   {
     id: int().primaryKey(),
-    name: text().notNull(), // primary key should be name + parent_id? - it will potentially eliminate possibility to create 2 direcotries with the same name in the same directory
+    name: text().notNull(),
     parentId: int("parent_id").references(
-      (): AnySQLiteColumn => directoriesTable.id
+      (): AnySQLiteColumn => directoriesTable.id,
+      { onDelete: "cascade" }
     ),
     privacy: text("privacy", { enum: ["private", "public", "unlisted"] })
       .notNull()
@@ -39,10 +60,29 @@ export const directoriesTable = sqliteTable(
       .default(sql`(strftime('%s', 'now'))`),
   },
 
-  (table) => [
+  (t) => [
     check(
       "privacy_enum_check",
-      sql`${table.privacy} IN ('private', 'public', 'unlisted')`
+      sql`${t.privacy} IN ('private', 'public', 'unlisted')`
     ),
+    unique().on(t.name, t.parentId),
   ]
 );
+
+export const promptsTable = sqliteTable("prompts", {
+  id: int().primaryKey(),
+  name: text(),
+  text: text().notNull(),
+  createdAt: int({ mode: "timestamp" })
+    .notNull()
+    .default(sql`(strftime('%s', 'now'))`),
+  modifiedAt: int("modified_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(strftime('%s', 'now'))`),
+});
+
+export const capturesAddedTable = sqliteTable("captures_added", {
+  id: int().primaryKey(),
+  date: text().notNull().unique(),
+  count: int().notNull().default(1),
+});
