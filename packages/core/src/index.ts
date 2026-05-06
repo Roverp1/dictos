9b7205568cc53eda0bc6cfedd23d1718a3e36ff0
Roverp <yar.zubaha@proton.me>
2026-05-06 12:53:26 +0200

    chore(core): export DirectoryService

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,6 @@ export * from "./models/";
 export * from "@ports/outbound/";
 
 export { CaptureService } from "@services/CaptureService";
+export { DirectoryService } from "@services/DirectoryService";
 
 export * from "./errors";
