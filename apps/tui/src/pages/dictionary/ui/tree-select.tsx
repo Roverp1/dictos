87f68b7a8370b53401f2845c2caeb07d2338c322
Roverp <yar.zubaha@proton.me>
2026-05-09 01:33:13 +0200

    feat(tui): allow user to rename treeItems on 'r'

--- apps/tui/src/pages/dictionary/ui/tree-select.tsx
+++ apps/tui/src/pages/dictionary/ui/tree-select.tsx
@@ -6,12 +6,21 @@ import type { TreeItem } from "../model/use-dictionary";
 interface TreeSelectProps extends ScrollBoxProps {
   items: TreeItem[];
   selectedIndex: number;
+
+  isRenaming?: boolean;
+  renameValue?: string;
+  onRenameChange?: (val: string) => void;
+  onRenameSubmit?: (val: string) => void;
 }
 
 export const TreeSelect = ({
   items,
   selectedIndex,
   focused,
+  isRenaming,
+  renameValue,
+  onRenameChange,
+  onRenameSubmit,
   ...props
 }: TreeSelectProps) => {
   const scrollBoxRef = useRef<ScrollBoxRenderable>(null);
@@ -22,14 +31,37 @@ export const TreeSelect = ({
       focused
       {...props}
     >
-      {items.map((item, i) => (
-        <text
-          key={item.id}
-          bg={selectedIndex === i ? "#78716c" : "#000"}
-        >
-          {item.label}
-        </text>
-      ))}
+      {items.map((item, i) => {
+        const isSelected = selectedIndex === i;
+        const bgColor = selectedIndex === i ? "#78716c" : "#000";
+
+        if (isSelected && isRenaming) {
+          return (
+            <box
+              key={item.id}
+              backgroundColor={bgColor}
+              paddingX={1}
+            >
+              <input
+                value={renameValue!}
+                onChange={onRenameChange!}
+                // @ts-expect-error opentui type bug
+                onSubmit={onRenameSubmit}
+                focused
+              />
+            </box>
+          );
+        }
+
+        return (
+          <text
+            key={item.id}
+            bg={bgColor}
+          >
+            {item.label}
+          </text>
+        );
+      })}
     </scrollbox>
   );
 };
