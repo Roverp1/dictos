fb84b5363aa07dceef27eaadfee910ff19ddfc82
MEDOYED <mmax26072005@gmail.com>
2026-05-15 18:20:22 +0200

    refactor(dictionary-page.tsx): move DirectoryTreePane and DefinitionPane to separate components

--- apps/tui/src/pages/dictionary/ui/dictionary-page.tsx
+++ apps/tui/src/pages/dictionary/ui/dictionary-page.tsx
@@ -1,17 +1,14 @@
-import {
+import { useDictionary } from "../model/use-dictionary";
+import { CreateModal } from "./modals/create-modal";
+import { DeleteConfirmModal } from "./modals/delete-confirm-modal";
+import { DirectoryTreePane } from "./directory-tree-pane";
+import { DefinitionPane } from "./definition-pane";
+
+import type {
   CaptureService,
   DirectoryService,
   DefinitionService,
 } from "@dictos/core";
-import { useRef } from "react";
-
-import { InteractiveList } from "./interactive-list";
-import { useDictionary } from "../model/use-dictionary";
-import { CreateModal } from "./modals/create-modal";
-import { DeleteConfirmModal } from "./modals/delete-confirm-modal";
-import { useTheme } from "@shared/lib/theme";
-import type { ScrollBoxRenderable } from "@opentui/core";
-import { SubmitTextarea } from "./submit-textarea";
 
 interface DictionaryPageProps {
   captureService: CaptureService;
@@ -24,8 +21,6 @@ export const DictionaryPage = ({
   directoryService,
   definitionService,
 }: DictionaryPageProps) => {
-  const theme = useTheme();
-
   const {
     itemsToDisplay,
     selectedIndex,
@@ -51,114 +46,24 @@ export const DictionaryPage = ({
       height="100%"
       flexDirection="row"
     >
-      <box
-        flexDirection="column"
-        id="directory-tree-pane"
-        border={["right"]}
-        borderColor={theme.base04}
-        width="50%"
-      >
-        <box marginBottom={0}>
-          <text fg={theme.base0D}>
-            {pathStack
-              .map((dir) => {
-                if (pathStack.length > 1 && dir.name === "/") return;
-                return dir.name;
-              })
-              .join("/")}
-          </text>
-        </box>
+      <DirectoryTreePane
+        pathStack={pathStack}
+        itemsToDisplay={itemsToDisplay}
+        focus={focus}
+        selectedIndex={selectedIndex}
+        setSelectedIndex={setSelectedIndex}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
 
-        {itemsToDisplay.length > 0 ? (
-          <InteractiveList
-            id="tree-item-list"
-            flexGrow={1}
-            items={itemsToDisplay}
-            focused={focus.pane === "tree"}
-            selectedIndex={selectedIndex}
-            onIndexChange={setSelectedIndex}
-            renderItem={(item, i, isSelected) => {
-              const isRenaming = focus.action === "renameInput";
-              const bgColor = isSelected ? theme.base02 : theme.base00;
-
-              if (isSelected && isRenaming) {
-                return (
-                  <box
-                    key={item.id}
-                    backgroundColor={bgColor}
-                    paddingX={1}
-                  >
-                    <input
-                      value={inputValue}
-                      onChange={setInputValue}
-                      // @ts-expect-error opentui type bug
-                      onSubmit={handleRenameSubmit}
-                      focused
-                    />
-                  </box>
-                );
-              }
-
-              return (
-                <text
-                  fg={theme.base06}
-                  bg={bgColor}
-                >
-                  {item.label}
-                </text>
-              );
-            }}
-          />
-        ) : (
-          <text>This directory is emty. Press 'a' to add you first item</text>
-        )}
-      </box>
-
-      <box
-        id="definition-pane"
-        width="50%"
-        height="100%"
-        flexDirection="column"
-        gap={1}
-      >
-        <text fg={focus.pane === "definition" ? theme.base0E : theme.base03}>
-          {selectedItem?.type === "capture" ? `${selectedItem.data.text}` : ""}
-        </text>
-
-        <InteractiveList
-          id="definition-list"
-          contentOptions={{ gap: 1 }}
-          flexGrow={1}
-          items={definitionsToDisplay}
-          focused={focus.pane === "definition"}
-          selectedIndex={defenitionIndex}
-          onIndexChange={setDefenitionIndex}
-          renderItem={(item, i, isSelected) => {
-            return (
-              <box
-                border
-                borderColor={isSelected ? theme.base0D : theme.base03}
-              >
-                <text fg={theme.base05}>{item.text}</text>
-              </box>
-            );
-          }}
-          ListFooterComponent={
-            focus.pane === "definition" && focus.action === "createInput" ? (
-              <box
-                border
-                borderColor={theme.base0B}
-              >
-                <SubmitTextarea
-                  focused={true}
-                  onSave={handleDefinitionSubmit}
-                />
-              </box>
-            ) : null
-          }
-        />
-        {/* <text>Press 'a' to add first definition for this capture</text> */}
-      </box>
+      <DefinitionPane
+        focus={focus}
+        selectedItem={selectedItem}
+        definitionsToDisplay={definitionsToDisplay}
+        defenitionIndex={defenitionIndex}
+        setDefenitionIndex={setDefenitionIndex}
+        handleDefinitionSubmit={handleDefinitionSubmit}
+      />
 
       {focus.pane === "tree" && focus.action === "createInput" && (
         <CreateModal
