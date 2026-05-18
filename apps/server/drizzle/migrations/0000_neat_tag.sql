3dbf422015c2dd3ee288b5b13e82a6c0654bfc7b
Roverp <yar.zubaha@proton.me>
2026-05-18 15:13:58 +0200

    feat(server): generate first migration

---
+++ apps/server/drizzle/migrations/0000_neat_tag.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `central_captures_added` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`user_id` integer NOT NULL,
+	`date` text NOT NULL,
+	`count` integer DEFAULT 1 NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `central_captures_added_date_unique` ON `central_captures_added` (`date`);--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`username` text NOT NULL,
+	`email` text NOT NULL,
+	`password_hash` text NOT NULL,
+	`bio` text,
+	`avatar_url` text,
+	`created_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`last_login_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
