630c7c1e17de1c57866735a7ff29227effcdf63b
Roverp <yar.zubaha@proton.me>
2026-05-21 10:06:50 +0200

    build(nix): use texliveFull to fix missing dependencies

--- flake.nix
+++ flake.nix
@@ -1,45 +1,23 @@
 {
-  description = "LaTeX compilation environment for Dictos report",
+  description = "LaTeX compilation environment for Dictos report";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  },
+  };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-
-      tex = pkgs.texlive.combine {
-        inherit (pkgs.texlive)
-          scheme-basic
-          latexmk
-          biber
-          biblatex
-          csquotes
-          geometry
-          setspace
-          indentfirst
-          titlesec
-          caption
-          footmisc
-          ragged2e
-          tocloft
-          newtx
-          babel-polish
-          xetex
-          fontspec
-          etoolbox;
-      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
-          tex
+          pkgs.texliveFull
         ];
 
         shellHook = ''
-          echo "LaTeX environment loaded."
+          echo "LaTeX environment loaded (texliveFull)."
           echo "To compile the report, run:"
           echo "cd docs/report && latexmk -pdf main.tex"
         '';
