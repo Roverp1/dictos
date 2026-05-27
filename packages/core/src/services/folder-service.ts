7642c51c6e7946ff3db2fc36856abdb6075d0be6
Roverp <yar.zubaha@proton.me>
2026-05-27 19:10:36 +0200

    chore(core): rm deprecated ports & getFolderTree method

--- packages/core/src/services/folder-service.ts
+++ packages/core/src/services/folder-service.ts
@@ -27,43 +27,6 @@ export class FolderService {
     return await this.repo.findByParentId(parentId);
   }
 
-  async getFolderTree(): Promise<FolderNode | DbError> {
-    const folders = await this.repo.findAll();
-    if (folders instanceof Error) return folders;
-
-    const nodeMap = new Map<number, FolderNode>();
-    let rootNode: FolderNode | undefined = undefined;
-
-    for (const folder of folders) {
-      const node: FolderNode = {
-        ...folder,
-        children: [],
-      };
-
-      nodeMap.set(folder.id, node);
-    }
-
-    for (const folder of folders) {
-      if (folder.parentId === null) {
-        rootNode = nodeMap.get(folder.id);
-
-        continue;
-      }
-
-      const parent = nodeMap.get(folder.parentId);
-      const currentNode = nodeMap.get(folder.id);
-
-      if (parent && currentNode) {
-        parent.children.push(currentNode);
-      }
-    }
-    if (!rootNode) {
-      throw new Error("Root folder not found in the database");
-    }
-
-    return rootNode;
-  }
-
   async renameFolder(
     id: string,
     newName: string
@@ -78,4 +41,3 @@ export class FolderService {
     return await this.repo.delete(id);
   }
 }
-
