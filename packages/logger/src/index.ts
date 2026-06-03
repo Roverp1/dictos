31b2d61a6372749900b8b1b19b93be54880f5823
Roverp <yar.zubaha@proton.me>
2026-06-03 20:35:43 +0200

    feat(packages): init logger interface

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,8 @@
+type Context = Record<string, unknown>;
+
+export interface Logger {
+  debug: (message: string, context?: Context) => void;
+  info: (message: string, context?: Context) => void;
+  warn: (message: string, context?: Context) => void;
+  error: (message: string, error?: Error | unknown, context?: Context) => void;
+}
