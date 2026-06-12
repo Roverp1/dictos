1c3f1536539b9a62299821bbcf70c39b70b66690
Roverp <yar.zubaha@proton.me>
2026-06-12 14:50:39 +0200

    chore: add gh to devenv

--- devenv.nix
+++ devenv.nix
@@ -13,7 +13,7 @@ in {
     TURSO_SYNC_URL = "http://localhost:8080";
   };
 
-  packages = with pkgs; [bun turso turso-cli secretspec];
+  packages = with pkgs; [bun turso turso-cli secretspec gh];
 
   processes = {
     turso-sync.exec = "tursodb ${serverDataDir}/sync-server.db --sync-server 0.0.0.0:8080";
