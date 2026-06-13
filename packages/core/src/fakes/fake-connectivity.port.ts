482e1e7f36d971dddace86bf826030ba958b7f27
Roverp <yar.zubaha@proton.me>
2026-06-13 09:27:05 +0200

    chore(core): clean up unfinished old tests

--- packages/core/src/fakes/fake-connectivity.port.ts
+++
@@ -1,9 +0,0 @@
-import type { ConnectivityPort } from "../ports/outbound";
-
-export class FakeConnectivityPort implements ConnectivityPort {
-  public simulateOffline = false;
-
-  async isOnline(): Promise<boolean> {
-    return !this.simulateOffline;
-  }
-}
